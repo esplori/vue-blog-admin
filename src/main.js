421146f6447029be5ca36b13173d3e5e90918f0d
vistol <2381773226@qq.com>
2019-04-06 20:14:14 +0800

    添加mavon-editor markdown编辑器

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/reset.css'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(mavonEditor)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
