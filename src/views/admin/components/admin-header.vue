95e00db5675502058513e2f804ef71471a108d65
vistol <2381773226@qq.com>
2019-06-30 10:28:25 +0800

    更新后台管理页面

---
+++ src/views/admin/components/admin-header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="admin-header">
+    <div>后台管理</div>
+    <div>欢迎：佚名</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'admin-header',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="less">
+  .admin-header {
+    padding: 20px;
+    background: #000;
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -1,28 +1,31 @@
 <!--后台管理首页-->
 <template>
   <div class="admin-index">
-    <el-row>
-      <el-col :span="4">
-        <el-menu default-active="/admin/pageList" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
-                 :collapse="isCollapse" :router="true" :unique-opened="false">
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span slot="title">控制台</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="/admin/pageList">文章管理</el-menu-item>
-              <el-menu-item index="/admin/post">发布文章</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-        </el-menu>
-      </el-col>
-      <el-col :span="20">
-        <div class="right-content">
-          <router-view></router-view>
-        </div>
-      </el-col>
-    </el-row>
+    <adminHeader></adminHeader>
+    <div class="container">
+      <el-row>
+        <el-col :span="4">
+          <el-menu default-active="/admin/pageList" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+                   :collapse="isCollapse" :router="true" :unique-opened="false">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span slot="title">控制台</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="/admin/pageList">文章管理</el-menu-item>
+                <el-menu-item index="/admin/post">发布文章</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+          </el-menu>
+        </el-col>
+        <el-col :span="20">
+          <div class="right-content">
+            <router-view></router-view>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
@@ -32,6 +35,9 @@ export default {
     return {
       isCollapse: false
     }
+  },
+  components: {
+    adminHeader: () => import('./components/admin-header')
   },
   methods: {
     handleOpen () {},
@@ -43,12 +49,23 @@ export default {
 <style scoped lang="less">
 
   .admin-index{
-    .el-menu-vertical-demo:not(.el-menu--collapse) {
-      width: 200px;
-      min-height: 400px;
-    }
-    .right-content{
-      padding: 40px;
+    height: 100%;
+    min-width: 1150px;
+    .container{
+      margin: 10px;
+      height: 100%;
+      .el-row{
+        height: 100%;
+        .el-col{
+          height: 100%;
+        }
+      }
+      .el-menu-vertical-demo {
+        height: 100%;
+      }
+      .right-content{
+        padding-left: 10px;
+      }
     }
   }
 
