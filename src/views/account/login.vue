44836fca9b4740845a98690f54459de1a9161f99
vistol <2381773226@qq.com>
2020-02-19 10:05:31 +0800

    update login enter event

--- src/views/account/login.vue
+++ src/views/account/login.vue
@@ -7,7 +7,7 @@
         <el-input v-model="form.username" placeholder="账号"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-input v-model="form.password" type="password" placeholder="密码"></el-input>
+        <el-input v-model="form.password" type="password" placeholder="密码" @keyup.enter.native="login"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button @click="login" type="primary" style="width:100%">登录</el-button>
