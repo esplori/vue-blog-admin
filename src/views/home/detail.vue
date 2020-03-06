f0dcbb4c171e6069cd0e6d4ceb7fc61f14e03783
vistol <2381773226@qq.com>
2020-03-06 20:44:23 +0800

    update style

--- src/views/home/detail.vue
+++ src/views/home/detail.vue
@@ -91,6 +91,14 @@ export default {
           display: block;
         }
       }
+      @media screen and(min-width: 769px) {
+        .pc{
+          display: block;
+        }
+        .mobile{
+          display: none;
+        }
+      }
     }
   }
 </style>
