fe77592394868b81d8b3839f386a69e4ebabf768
vistol <2381773226@qq.com>
2020-10-25 22:42:19 +0800

    删除vue-meta-info

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
-import MetaInfo from 'vue-meta-info' // 引入vue-meta-info设置标题
 import mavonEditor from 'mavon-editor'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/reset.css'
@@ -12,7 +11,6 @@ import '@/assets/css/common.css'
 import 'mavon-editor/dist/css/index.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI, {size: 'medium'})
-Vue.use(MetaInfo)
 Vue.use(mavonEditor)
 /* eslint-disable no-new */
 new Vue({
